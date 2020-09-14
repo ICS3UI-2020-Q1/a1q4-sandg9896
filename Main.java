3148e70101530f42049cba78238234394666979d
sandg9896 <sandg9896@wrdsb.ca>
2020-09-14 16:14:28 +0000

    Slope

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Gavin Sandhar
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // help grade 9s calculate the slope of a line given two points
+    String name;
+   System.out.println("Enter value for X1:");
+    // Creates a Scanner used for input
+    Scanner input = new Scanner(System.in);
+     double X1 = input.nextInt();
+    System.out.println("Enter value for Y1:");
+     double Y1 = input.nextInt();
+    System.out.println("Enter value for X2:");
+     double X2 = input.nextInt();
+    System.out.println("Enter value for Y2:");
+     double Y2 = input.nextInt();
+
+      System.out.println("The slope of the line is " +  (Y2 -Y1)/(X2 - X1) );
+
+
   }
 }
